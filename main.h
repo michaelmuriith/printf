25be38ce9506e0af7d60f5d89f4a0aeba6ee829b
root <japhethmuli.mj@gmail.com>
2022-08-10 11:16:09 -0700

    main.h

--- main.h
+++ main.h
@@ -4,87 +4,136 @@
 
 
 
-#include <stdio.h>
-
 #include <stdlib.h>
 
 #include <stdarg.h>
 
-#include <limits.h>
-
-#include <unistd.h>
 
 
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
 
+typedef struct flags
 
+{
+
+	int plus;
+
+	int space;
+
+	int hash;
+
+} flags_t;
 
 
 
 /**
- * struct format - match the conversion specifiers for printf
- * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @f: type pointer to function for the conversion specifier
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
  */
 
-
-
-typedef struct format
+typedef struct printHandler
 
 {
 
-	char *id;
+	char c;
 
-	int (*f)();
+	int (*f)(va_list ap, flags_t *f);
 
-} convert_match;
+} ph;
 
 
 
-int printf_pointer(va_list val);
+/* print_nums */
 
-int printf_hex_aux(unsigned long int num);
+int print_int(va_list l, flags_t *f);
 
-int printf_HEX_aux(unsigned int num);
+void print_number(int n);
 
-int printf_exclusive_string(va_list val);
+int print_unsigned(va_list l, flags_t *f);
 
-int printf_HEX(va_list val);
+int count_digit(int i);
 
-int printf_hex(va_list val);
 
-int printf_oct(va_list val);
 
-int printf_unsigned(va_list args);
+/* print_bases */
 
-int printf_bin(va_list val);
+int print_hex(va_list l, flags_t *f);
 
-int printf_srev(va_list args);
+int print_hex_big(va_list l, flags_t *f);
 
-int printf_rot13(va_list args);
+int print_binary(va_list l, flags_t *f);
 
-int printf_int(va_list args);
+int print_octal(va_list l, flags_t *f);
 
-int printf_dec(va_list args);
 
-int _strlen(char *s);
 
-int *_strcpy(char *dest, char *src);
+/* converter */
 
-int _strlenc(const char *s);
+char *convert(unsigned long int num, int base, int lowercase);
 
-int rev_string(char *s);
 
-int _strlenc(const char *s);
 
-int printf_37(void);
+/* _printf */
 
-int printf_char(va_list val);
+int _printf(const char *format, ...);
 
-int printf_string(va_list val);
+
+
+/* get_print */
+
+int (*get_print(char s))(va_list, flags_t *);
+
+
+
+/* get_flag */
+
+int get_flag(char s, flags_t *f);
+
+
+
+/* print_alpha */
+
+int print_string(va_list l, flags_t *f);
+
+int print_char(va_list l, flags_t *f);
+
+
+
+/* write_funcs */
 
 int _putchar(char c);
 
-int _printf(const char *format, ...);
+int _puts(char *str);
+
+
+
+/* print_custom */
+
+int print_rot13(va_list l, flags_t *f);
+
+int print_rev(va_list l, flags_t *f);
+
+int print_bigS(va_list l, flags_t *f);
+
+
+
+/* print_address */
+
+int print_address(va_list l, flags_t *f);
+
+
+
+/* print_percent */
+
+int print_percent(va_list l, flags_t *f);
 
 
 
